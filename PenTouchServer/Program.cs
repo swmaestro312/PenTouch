eba4b4e810434f05ea93248f8f3483e16bfe8491
CodeAthei <code.athei@gmail.com>
2012-11-04 11:33:01 +0900

    Server

---
+++ PenTouchServer/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace PenTouchServer
+{
+	class Program
+	{
+		private static Mutex mut = new Mutex();
+		private static List<TcpClient> array;
+		//private static int clientNum;
+
+		static void Main(string[] args)
+		{
+			//clientNum = 0;
+			array = new List<TcpClient>();
+			TcpListener socketListener = new TcpListener(21122);
+			socketListener.Start();
+			Console.WriteLine("begin accept");
+			socketListener.BeginAcceptSocket(new AsyncCallback(DoAcceptSocketCallback), socketListener);
+			while (true)
+			{
+				Console.WriteLine("in while loop");
+				Thread.Sleep(1000);
+				mut.WaitOne();
+				{
+					Console.WriteLine("client number : " + array.Count);
+					for (int i = 0; i < array.Count(); i++)
+					{
+						NetworkStream stream;
+						try
+						{
+							stream = array[i].GetStream();
+						}
+						catch (Exception E)
+						{
+							Console.WriteLine("Stream make failed");
+							array.RemoveAt(i);
+							i--;
+							continue;
+						}
+						Byte[] bytes = new Byte[4096];
+						int count;
+						stream.ReadTimeout = 100;
+						String data = "";
+						try
+						{
+							while ((count = stream.Read(bytes, 0, bytes.Length)) != 0) 
+							{
+								data += System.Text.Encoding.ASCII.GetString(bytes, 0, count);
+								Console.WriteLine(i + " : " + data);
+							}
+						}
+						catch (Exception E)
+						{
+							//Console.WriteLine(i + " : " + E.Message);
+						}
+						if (data.Length != 0)
+						{
+							byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+							for (int j = 0; j < array.Count(); j++)
+							{
+								if (i != j)
+								{
+									try
+									{
+										array[j].GetStream().Write(msg, 0, msg.Length);
+									}
+									catch (Exception E)
+									{
+										Console.WriteLine("write failed");
+										array.RemoveAt(j);
+										j--;
+									}
+								}
+							}
+						}
+					}
+				}
+				mut.ReleaseMutex();
+			}
+		}
+
+		public static void DoAcceptSocketCallback(IAsyncResult ar)
+		{
+			Console.WriteLine("accept callback called");
+			TcpListener socketListener = (TcpListener)ar.AsyncState;
+			mut.WaitOne();
+			{
+				Console.WriteLine("callback got mutex");
+				TcpClient client = socketListener.EndAcceptTcpClient(ar);
+				array.Add(client);
+				//clientNum++;
+				socketListener.BeginAcceptSocket(new AsyncCallback(DoAcceptSocketCallback), socketListener);
+			}
+			mut.ReleaseMutex();
+		}
+	}
+}
